fbfe56338ff3037231ae0c439a435e337b048d43
Andrewtheprince <andry.mazzo@outlook.com>
2025-06-04 12:02:25 +0200

    first commit

--- database/DAO.py
+++ database/DAO.py
@@ -1,6 +1,18 @@
 from database.DB_connect import DBConnect
 
+class DAO:
 
-class DAO():
-    def __init__(self):
-        pass
+    @staticmethod
+    def getStore():
+        conn = DBConnect.get_connection()
+        cursor = conn.cursor(dictionary = True)
+        result = []
+        query = """ SELECT 
+                    FROM """
+        cursor.execute(query)
+        for row in cursor:
+            result.append(row)
+        cursor.close()
+        conn.close()
+        return result
+    
